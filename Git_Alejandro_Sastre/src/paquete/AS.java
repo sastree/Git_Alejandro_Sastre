803ce67aa473c5d88b1214f5d61cc50d9655f57a
Alejandro <sastre212324@gmail.com>
2022-12-28 18:43:18 +0100

    Modificacion 3

--- Git_Alejandro_Sastre/src/paquete/AS.java
+++ Git_Alejandro_Sastre/src/paquete/AS.java
@@ -4,7 +4,7 @@ public class AS {
 
 	public static void main(String[] args) {
 		System.out.println("Alumno: Alejandro Sastre");
-		System.out.println("Primera Modificacion desde Master");
+		System.out.println("Modificacion desde Master - Cambiado");
 	}
 
 }
